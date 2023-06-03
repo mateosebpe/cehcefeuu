72356a1ac98812c8fe2f01811918dc602f8f94e4
mateosebpe <mateosebpe@gmail.com>
2023-06-03 05:27:21 -0300

    Ver archivos en lista

--- public/scripts/storageBajar.js
+++ public/scripts/storageBajar.js
@@ -1,6 +1,6 @@
-
 function bajarStorage() {
     var fileCategory = document.getElementById('downloadCategory').value;
+    var filesInHTML = ``;
     // Obtén una referencia al almacenamiento de Firebase
     var storageRef = firebase.storage().ref();
 
@@ -18,10 +18,22 @@ function bajarStorage() {
                 console.log('Nombre del archivo:', fileName);
                 console.log('URL de descarga:', downloadURL);
                 // Realiza aquí las acciones correspondientes con el archivo
+                filesInHTML += `<div class="list-group-item d-flex w-100 justify-content-between" p-2 my-2">
+            <h5 class="w-100">${fileRef.name}</h5>
+            <a href="${downloadURL}" class="mx-4">Ver</a>
+            <button class="btn btn-outline-primary btn-sm">Eliminar</button>
+        </div>`;
+        console.log(filesInHTML);
+        let viewList = document.getElementById('viewFiles');
+        viewList.innerHTML = filesInHTML;
             });
+
+            
         });
     }).catch(function (error) {
         // Se ejecuta si hay un error al obtener la lista de archivos
         console.log('Error al obtener la lista de archivos:', error);
     });
+    
+
 }
